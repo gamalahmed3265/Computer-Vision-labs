b80b885a125e59eaf734854f76dd613469296db1
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-23 09:36:09 +0300

    add preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -6,13 +6,15 @@ import os
 def isExists(path: str):
     assert os.path.exists(path)
     
-def resize(img :np.ndarray,scale_percent=30):
-    #img=cv.resize(img,(550,250)) # w,h
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-    #dsize
-    dsize = (width, height)
-    img = cv.resize(img, dsize)
+def resize(img :np.ndarray,scale_percent=30,flag:bool=True):
     
+    if flag:
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        #dsize
+        dsize = (width, height)
+        img = cv.resize(img, dsize)
+    else:
+        img=cv.resize(img,(512,512)) # w,h
     return img
     
