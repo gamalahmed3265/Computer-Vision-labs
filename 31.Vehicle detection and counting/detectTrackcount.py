a9624b9e31cf9cd35a4c5ecb0652b7c2b5fc328f
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-23 19:03:47 +0300

    add 31.Vehicle detection and counting

---
+++ 31.Vehicle detection and counting/detectTrackcount.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+from preprocessing import grayImage,resize
+from tracker import TrackerMaster
+
+track=TrackerMaster()
+# font=cv2.FONT_HERSHEY_PLAIN
+font=cv2.FONT_HERSHEY_SIMPLEX
+
+BGS= cv2.createBackgroundSubtractorMOG2(history=100)
+
+
+ww=80
+hh=80
+
+detec=[]
+offset=6
+y1=300
+
+def page_center(x,y,w,h):
+    h=int(h/2)
+    w=int(w/2)
+    cx=x+h
+    cy=y+w
+    return cx,cy
+
+def DetectionTracking(frame: np.ndarray,count:int):
+    gray=grayImage(frame)
+    #blur
+    blur=cv2.GaussianBlur(gray,(3,3),5)
+    #subtrationg
+    img_sub=BGS.apply(blur)
+    
+    dilat=cv2.dilate(img_sub,np.ones((5,5)))
+    
+    contour,hierarchy=cv2.findContours(dilat,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+        
+    cv2.line(frame,(25,y1),(1200,y1),(255,0,0),3)
+    
+    for (i,c) in enumerate(contour):
+        (x,y,w,h)=cv2.boundingRect(c)
+        
+        valider_contorno=(w>=ww)and (h>=hh)
+        
+        if not valider_contorno:
+            continue
+        
+        cv2.rectangle(frame,(x,x),(x+w,y+h),(0,255,0),2)
+        center=page_center(x, y, w, h)
+    
+        detec.append(center)
+    
+        cv2.circle(frame,center,4,(0,0,255),-1)
+        
+        for (x,y) in detec:
+            if y<(y1+offset) and y>(y1-offset):
+                count+=1
+                cv2.line(frame,(25,y1),(1200,y1),(172,88,3),3)
+                detec.remove((x,y))
+                print(f"No. cars{count}")
+                
+    cv2.putText(frame,f"Nm. {count}",(25,y1),font,1,(244,45,2),4)
+
+
+    cv2.imshow("frame",frame)
+    # cv2.imshow("gray",gray)
+    # cv2.imshow("blur",blur)
+    cv2.imshow("img_sub",img_sub)
+    
+
+
+
+
+
---
+++ 31.Vehicle detection and counting/main.py
@@ -0,0 +1,58 @@
+import cv2 as cv
+import numpy as np
+import os
+from preprocessing import isExists,resize,getDimVideo,clickEventImg
+from detectTrackcount import DetectionTracking
+
+url="assets"
+# imageName="early.png"
+# imageName="orange.png"
+# imageName2="apple.png"
+# imageName="contours.png"
+# imageName="logo.png"
+
+# videoName="pepole.mp4"
+videoName="highway.mp4"
+videoName="Vehicle detection and counting.mp4"
+
+path=f"{url}/{videoName}"
+
+isExists(path)
+
+# img=cv.imread(path)
+
+# img=resize(img,scale_percent=60)
+
+# print(img.shape,img2.shape)
+
+
+cap=cv.VideoCapture(path)
+
+# getDimVideo(cap)
+
+
+
+count=0
+while True:
+    rat,frame=cap.read()
+    if frame is None:
+        break
+    
+    frame=resize(frame,flag=False,size=(750,450))
+    
+    DetectionTracking(frame,count)
+
+    cv.imshow("frame",frame)
+    
+    # cv.setMouseCallback('frame',clickEventImg)
+    
+    if cv.waitKey(60)==27:
+        break
+    
+cv.destroyAllWindows()
+cap.release()
+
+
+
+# cv.imshow("img",img)
+
