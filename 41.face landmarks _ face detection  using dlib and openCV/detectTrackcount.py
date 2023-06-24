2f4d08d17725456c4aa839d2900f2d4dae96ebe8
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-24 12:14:56 +0300

    add 41.face landmarks _ face detection  using dlib

---
+++ 41.face landmarks _ face detection  using dlib and openCV/detectTrackcount.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from preprocessing import grayImage,resize
+from tracker import TrackerMaster
+
+import dlib
+
+print(dlib.__version__)
+
+track=TrackerMaster()
+# font=cv2.FONT_HERSHEY_PLAIN
+# font=cv2.FONT_HERSHEY_SIMPLEX
+font=cv2.FONT_HERSHEY_COMPLEX
+
+
+detector=dlib.get_frontal_face_detecor()
+predictor=dlib.shape_predictor("shape_predicor_68_face_landmarks.dat")
+
+
+
+def DetectionTracking(frame: np.ndarray, roi:np.ndarray):
+    gray=grayImage(frame)
+    faces=detector(gray)
+    for face in faces:
+        x1=face.left()
+        y1=face.top()
+        x2=face.righs()
+        y2=face.bottom()
+        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),3)
+        landmarks=predictor(gray,face)
+        for n in range(0,68):
+            x=landmarks.part(n).x
+            y=landmarks.part(n).y
+            cv2.circle(frame,(x,y),3,(0,255,0),-1)
+            
+    cv2.imshow("frame",frame)    
+        
+
+
+
+
+
---
+++ 41.face landmarks _ face detection  using dlib and openCV/main.py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+from preprocessing import *
+from detectTrackcount import DetectionTracking
+
+# url="assets"
+
+# imageName="early.png"
+# imageName="orange.png"
+# imageName2="apple.png"
+# imageName="contours.png"
+# imageName="logo.png"
+# imageName="Geometric Shapes .png"
+# videoName="pepole.mp4"
+# videoName="highway.mp4"
+# videoName="Vehicle detection and counting.mp4"
+videoName="eyes.mp4"
+path=f"{url}/{videoName}"
+
+isExists(path)
+
+######### img #########
+
+# img=cv.imread(path)
+
+# img=resize(img,scale_percent=60)
+
+# print(img.shape,img2.shape)
+
+
+# DetectionTracking(img)
+
+# cv.imshow('face',img)
+# closeImg()
+
+
+
+
+
+
+
+
+######### video #########
+cap=cv.VideoCapture(path)
+
+# w,h=getDimVideo(cap)
+
+
+
+while True:
+    rat,frame=cap.read()
+    if rat is False:
+        break
+    frame=resize(frame,scale_percent=60)
+    
+    DetectionTracking(frame,roi)
+    # cv.imshow("frame",frame)
+
+    # cv.setMouseCallback('frame',clickEventImg)
+
+    if cv.waitKey(60)==27:
+        break
+    
+cv.destroyAllWindows()
+cap.release()
+
+
+
+
