fbaf7c4756380c6da58f225d6e71cbf4cdf770cb
gamal ahmed <gamalahmed3265@gmail.com>
2023-06-25 12:44:30 +0300

    add 46.text recognation from image by using pytesseract

--- 46.text recognation from image by using pytesseract OCR _ Computer vision/detectTrackcount.py
+++ 46.text recognation from image by using pytesseract OCR _ Computer vision/detectTrackcount.py
@@ -4,21 +4,21 @@ from preprocessing import *
 from tracker import TrackerMaster
 import mediapipe as mp
 import time 
-# import pytesseract
+import pytesseract
 
 track=TrackerMaster()
 
 font=getFonts()
-# pytesseract.pytesseract.tesseract_cmd="dir"
+pytesseract.pytesseract.tesseract_cmd="dir"
 
 
 def DetectionTracking(frame: np.ndarray):
     gray=grayImage(frame)
     thr=cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,85,11)
     config="--psm 3" #page segmentation
-    # pytesseract.pytesseract.tesseract_cmd
-    # text=pytesseract.image_to_string(img ,config ,lan="eng)
-    # print(text)
+    pytesseract.pytesseract.tesseract_cmd
+    text=pytesseract.image_to_string(img ,config ,lan="eng)
+    print(text)
     
     cv2.imshow("frame",frame)
     cv2.imshow("frame",thr)
